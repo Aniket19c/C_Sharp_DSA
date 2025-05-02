5560489b6d1c7aa5f8dc767afc66c94caa7aeb18
Aniket19c <aniketchaudhary19j@gmail.com>
2025-05-02 22:22:16 +0530

    Solved Trapped water problem using auxiliary arrays

--- Program.cs
+++ Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace DSA_Solutions
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Enter the height values separated by spaces:");
+            string input = Console.ReadLine();
+
+            int[] height = input.Split(' ')
+                                .Where(s => !string.IsNullOrWhiteSpace(s))
+                                .Select(int.Parse)
+                                .ToArray();
+
+            TrappedWater solution = new TrappedWater();
+            int result = solution.Trap(height);
+
+            Console.WriteLine($"Trapped water: {result}");
         }
     }
 }
---
+++ TrappedWater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DSA_Solutions
+{
+    public class TrappedWater
+    {
+        public int Trap(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int leftMax = height[left];
+            int rightMax = height[right];
+            int water = 0;
+
+            while (left < right)
+            {
+                if (leftMax < rightMax)
+                {
+                    left++;
+                    leftMax = Math.Max(leftMax, height[left]);
+                    water += leftMax - height[left];
+                }
+                else
+                {
+                    right--;
+                    rightMax = Math.Max(rightMax, height[right]);
+                    water += rightMax - height[right];
+                }
+            }
+
+            return water;
+        }
+    
+    }
+}
